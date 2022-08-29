4a788a3b65934477cb2ba4f4505769ee7dc9dce3
Akhil Dhanesh <akhildhanesh1@outlook.com>
2022-08-29 05:42:21 +0530

    v 1.0

--- id.js
+++ id.js
@@ -1,3 +1,5 @@
+'use strict'
+
 module.exports = {
     id: () => {
         const d = new Date()
--- mongo.js
+++ mongo.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const { MongoClient } = require('mongodb');
 const { id } = require('./id')
 
