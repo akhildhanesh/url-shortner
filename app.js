0ea4a809a40bd3508961804c59fa6f2242b1bc49
Akhil Dhanesh <akhildhanesh1@outlook.com>
2022-08-31 20:52:33 +0530

    null check

--- app.js
+++ app.js
@@ -27,11 +27,13 @@ app.get('/:url', async (req, res) => {
     if (data != null) return res.redirect(data)
     db.getUrl(req.params.url)
         .then(result => {
+            if (result.length === 0) return res.send('Something\'s wrong here')
             res.redirect(result[0]['url'])
             client.set(req.params.url, result[0]['url'], 'EX', 60 * 60 * 24)
         })
         .catch(err => console.log(err))
 })
 
+app.get('*', (req, res) => res.send('err'))
 
 app.listen(PORT, () => console.log(`running on port => ${PORT}`))
